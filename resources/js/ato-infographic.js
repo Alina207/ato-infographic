e5a8324b309192496709eb372b8c237dfc8790db
Gasperino, Alina <Alina.Gasperino@transunion.com>
2018-11-01 17:22:10 -0400

    Added animations to sections 3, 5, 6

--- resources/js/ato-infographic.js
+++ resources/js/ato-infographic.js
@@ -1,20 +1,3 @@
-/* Smooth Scroll --swapped out the "a" to "area" which is used in the image navigation at top */
-$(function () {
-	$('area[href*="#"]:not([href="#"])').click(function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: target.offset().top - 75
-				}, 1000);
-				return false;
-			}
-		}
-	});
-});
-
-
 /*on window finish loading the animations can start based on scroll into and out of sections */
 
 window.onload = function onLoad() {
@@ -22,168 +5,187 @@ window.onload = function onLoad() {
 	//hiding load wheel when page is finished loading
 	$('#loader-wrapper').hide();
 
-		var options = {
-			useEasing: false,
-			  useGrouping: true,
-			suffix: '%'
-		};
+	///////////////////////////////////////
+	// Section 1 - Intro
+	///////////////////////////////////////
 
-		var images = [
-			"./resources/img/sections/intro/ATO-Infographic-1-header.jpg",
-			"./resources/img/sections/intro/ATO-Infographic-1-header2.jpg",
-			"./resources/img/sections/intro/ATO-Infographic-1-header3.jpg"
-		  ]
-		  
-		  var imageHead = document.getElementById("intro-waypoint");
-		  var i = 0;
-		  
-		  setInterval(function() {
-				imageHead.style.backgroundImage = "url(" + images[i] + ")";
-				i++;
-				if (i == images.length) {
-					i =  0;
-				}
-		  }, 800);
+	// Swaps out header images
+	var images = [
+		"./resources/img/sections/intro/ATO-Infographic-1-header.jpg",
+		"./resources/img/sections/intro/ATO-Infographic-1-header2.jpg",
+		"./resources/img/sections/intro/ATO-Infographic-1-header3.jpg"
+	]
+
+	var imageHead = document.getElementById("intro-waypoint");
+	var i = 0;
+
+	setInterval(function () {
+		imageHead.style.backgroundImage = "url(" + images[i] + ")";
+		i++;
+		if (i == images.length) {
+			i = 0;
+		}
+	}, 800);
+
+
+	// Calculate the viewport height
+	var viewHeight = $(window).height();
+	var windowWidth = $(window).width();
+
+	// Set min height for small desktop + up
+	if ((viewHeight) > 991) {
+		$(".section-intro").css({
+			'min-height': viewHeight - 40
+		});
+	}
+	$(window).on('resize', function () {
+		var viewHeight = $(window).height();
+		var windowWidth = $(window).width();
+		console.log(windowWidth);
+		// Set min height for small desktop + up
+		if (viewHeight > 991) {
+			$(".section-intro").css({
+				'min-height': viewHeight - 40
+			});
+		} else {
+			$(".section-intro").css({
+				'min-height': 'auto'
+			});
+		}
+	});
+
+	var introWaypoint = new Waypoint.Inview({
+		element: document.getElementById('intro-waypoint'),
+		enter: function () {
+			console.log("Enter intro waypoint");
+			// animations here
+		},
+		exited: function () {
+			console.log("Exit intro waypoint");
+			
+		}
+	});
+
+	///////////////////////////////////////
+	// Section 2 - What is ATO
+	///////////////////////////////////////
+
+	var whatIsATOWaypoint = new Waypoint.Inview({
+		element: document.getElementById('what-is-ato'),
+		enter: function (direction) {
+			console.log("Enter 'What is ATO' waypoint");
+			$('.fish-hook').addClass('swing');
+		},
+		exited: function (direction) {
+			console.log("Exit 'What is ATO' waypoint");
+		}
 		
-
-			///////////////////////////////////////
-			// Section 1 - Intro
-			///////////////////////////////////////
-
-			// fade in intro iphone image after load wheel hides
-			$('.fadein').fadeIn(3000);
-			//allows for image navigation
-			$('img[usemap]').rwdImageMaps();
-
-			// Calculate the viewport height
-			var viewHeight = $(window).height();
-			var windowWidth = $(window).width();
-
-			// Set min height for small desktop + up
-			if ((viewHeight) > 991) {
-				$(".section-intro").css({
-					'min-height': viewHeight - 40
-				});
-			}
-			$(window).on('resize', function () {
-				var viewHeight = $(window).height();
-				var windowWidth = $(window).width();
-				console.log(windowWidth);
-				// Set min height for small desktop + up
-				if (viewHeight > 991) {
-					$(".section-intro").css({
-						'min-height': viewHeight - 40
-					});
-				} else {
-					$(".section-intro").css({
-						'min-height': 'auto'
-					});
-				}
-			});
-
-			var landingWaypoint = new Waypoint.Inview({
-				element: document.getElementById('intro-waypoint'),
-				enter: function () {
-					// any intro animations
-				},
-				exited: function () {
-					// any intro animations
-				}
-			});
-
-			///////////////////////////////////////
-			// Section 2 - What is ATO
-			///////////////////////////////////////
-
-			var stat1 = new CountUp("stat-1", 0, 80, 0, 2, options);
-			var stat2 = new CountUp("stat-2", 0, 20, 0, 2, options);
-
-			var statWaypoint1 = new Waypoint({
-				element: document.getElementById('what-is-ato'),
-				handler: function () {
-					stat1.start();
-					stat2.start();
-				},
-				offset: 'bottom-in-view'
-			});
-
-			var trafficWaypoint = new Waypoint.Inview({
-				element: document.getElementById('what-is-ato'),
-				enter: function () {
-					// any car animations
-				},
-				exited: function () {
-					// any car animations
-				}
-			});
+	});
 
 
 
-			//////////////////////////////////
-			// Section 3 - The ROI of ATO
-			//////////////////////////////////
+	//////////////////////////////////
+	// Section 3 - The ROI of ATO
+	//////////////////////////////////
 
-			//Animate barchart
-			var barWaypoint = new Waypoint({
-				element: document.getElementById('bar-waypoint'),
-				handler: function () {
-					$('.bar').addClass('animate');
-					$('.bar p').addClass('fade-in');
-				},
-				offset: 'bottom-in-view'
-			});
+	// Animate barchart
+	// var barWaypoint = new Waypoint({
+	// 	element: document.getElementById('bar-waypoint'),
+	// 	handler: function () {
+	// 		console.log("the ROI of ATO Waypoint");
+	// 		$('.first-arm').addClass('animate');
+	// 		$('.second-arm').addClass('animate');
+	// 		$('.third-arm').addClass('animate');
+	// 	},
+	// 	offset: 'bottom-in-view'
+	// });
 
-			
-
-			/////////////////////////////////////
-			// Section 4 - The Impact of Account Takeover Fraud
-			/////////////////////////////////////
-
-			// Initiate bar stats
-			var stat8 = new CountUp("stat-8", 0, 30, 0, 2, options);
-			var stat9 = new CountUp("stat-9", 0, 63, 0, 2, options);
-			var stat10 = new CountUp("stat-10", 0, 74, 0, 2, options);
-
-			//Animate Impact section
-			var impactWaypoint = new Waypoint({
-				element: document.getElementById('impact-waypoint'),
-				handler: function () {
-					/*Stats*/
-					stat8.start();
-					stat9.start();
-					stat10.start();
-				},
-				offset: 'bottom-in-view'
-			});
-
-
-			/////////////////////////////////////
-			// Section 5 - Mobile Payment Fraud
-			////////////////////////////////////
-
-			var mobileWaypoint = new Waypoint.Inview({
-				element: document.getElementById('mobile-fraud'),
-				entered: function (direction) {
-					/*fadein*/
-					$('.bottom-phone').fadeIn(3000);
-					$('#phone-cta').fadeIn(4000);
-				},
-				exited: function (direction) {
-					// stat animations
-
-				}
-			});
-
-			/////////////////////////////////////
-			// Section 6 - User Behavior Patterns
-			/////////////////////////////////////
+	
+	var barWaypoint = new Waypoint.Inview({
+		element: document.getElementById('bar-waypoint'),
+		enter: function () {
+			console.log(" Enter the ROI of ATO Waypoint");
+			$('.first-arm').addClass('animate');
+			$('.second-arm').addClass('animate');
+			$('.third-arm').addClass('animate');
+		},
+		exited: function () {
+			console.log("Exit the ROI of ATO Waypoint");
+		}
+	});
 
 
 
-			/////////////////////////////////////
-			// Section 7 - Get the Insight Guide
-			////////////////////////////////////
+	/////////////////////////////////////
+	// Section 4 - The Impact of Account Takeover Fraud
+	/////////////////////////////////////
+
+	//Animate Impact section
+	var impactWaypoint = new Waypoint({
+		element: document.getElementById('impact-waypoint'),
+		handler: function () {
+			console.log("Impact waypoint");
+		},
+		offset: 'bottom-in-view'
+	});
 
 
-			
+	/////////////////////////////////////
+	// Section 5 - Mobile Payment Fraud
+	////////////////////////////////////
+
+	var mobileWaypoint = new Waypoint.Inview({
+		element: document.getElementById('mobile-fraud'),
+		entered: function () {
+			console.log("Enter mobile fraud waypoint");
+			$('.left-phone').addClass('come-in');
+		},
+		exited: function () {
+			console.log("Exit mobile fraud waypoint");
+			// animations here
+		}
+	});
+
+	/////////////////////////////////////
+	// Section 6 - User Behavior Patterns
+	/////////////////////////////////////
+
+	var userBehaviorWaypoint = new Waypoint.Inview({
+		element: document.getElementById('user-behavior'),
+		entered: function (direction) {
+			console.log("Enter user behavior waypoint");
+			$('.svg').addClass('come-in');
+		},
+		exited: function (direction) {
+			console.log("Enter user behavior waypoint");
+			// animations here
+		}
+	});
+
+
+
+	/////////////////////////////////////
+	// Section 7 - Get the Insight Guide
+	////////////////////////////////////
+
+	var insightGuideWaypoint = new Waypoint.Inview({
+		element: document.getElementById('insight-guide'),
+		entered: function (direction) {
+			console.log("Enter insight guide waypoint");
+		},
+		exited: function (direction) {
+			console.log("Enter insight guide waypoint");
+			// animations here
+		}
+	});
+
+
 }; //end window onLoad
+
+
+/* FYI: 
+offset: 'bottom-in-view' means when the bottom of said item is in view 
+        do the behavior 
+entered/exited: allows for a behavior of something when it's in the view port as opposed to enter or exit which might take place before it's in view
+				i.e the div may start at the bottom of the vp
+*/
